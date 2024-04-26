4d878983964c87fd3c1fe446f1fc4fdd5081b610
Evandro Xavier <exavier@MacBook-Air-de-Evandro.local>
2024-04-25 23:00:01 -0300

    Mapeamento com annotations JPA e correção do arquivo 'application properties'

--- src/main/java/com/estudosjava/atendimentomedico/entities/Paciente.java
+++ src/main/java/com/estudosjava/atendimentomedico/entities/Paciente.java
@@ -3,9 +3,20 @@ package com.estudosjava.atendimentomedico.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_paciente")
 public class Paciente implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String matricula;
 	private String name;
