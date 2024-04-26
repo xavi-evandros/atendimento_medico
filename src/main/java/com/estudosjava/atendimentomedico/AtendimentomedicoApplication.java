7abcdc080aa5afa6769b66222c24de7fffb52aa6
Evandro Xavier <exavier@MacBook-Air-de-Evandro.local>
2024-04-25 21:49:19 -0300

    Projeto criado

---
+++ src/main/java/com/estudosjava/atendimentomedico/AtendimentomedicoApplication.java
@@ -0,0 +1,13 @@
+package com.estudosjava.atendimentomedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendimentomedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendimentomedicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudosjava/atendimentomedico/AtendimentomedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudosjava.atendimentomedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtendimentomedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
